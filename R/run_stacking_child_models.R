7f3651101dc99265a2389e28a68893e526b8aa7a
Daniel Casey <dccasey@uw.edu>
2017-05-31 19:15:27 -0700

    change to mclapply framework for run_stacking_models

--- R/run_stacking_child_models.R
+++ R/run_stacking_child_models.R
@@ -34,6 +34,15 @@ run_stacking_child_models = function(st){
     #})
 
   #run the models
+  stacking_models = parallel::mclapply(1:nrow(model_grid),
+                                      function(x) get(paste0('fit_',get_model_type(st, model_grid[x,get('model_name')])))(
+                                        st = st,
+                                        model_name = model_grid[x,get('model_name')],
+                                        fold_col = model_grid[x,get('fold_columns')],
+                                        fold_id = model_grid[x,get('fold_ids')],
+                                        return_model_obj = model_grid[x,get('return_model_obj')]),
+                                      mc.cores = st$general_settings$cores,mc.preschedule = F)
+  #attempt at parlapply
   # stacking_models = parallel::parLapplyLB(clus, 1:nrow(model_grid),
   #                   function(x) get(paste0('fit_',get_model_type(st, model_grid[x,get('model_name')])))(
   #                     st = st,
